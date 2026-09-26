08cc0740e70c915035808c4f368948195dcf12d3
Weiqi Zhang <weiqi.zhang_arthur@yahoo.com>
2026-09-26 21:19:52 +0200

    finish sorted_arrays_merge

--- epi_judge_cpp/sorted_arrays_merge.cc
+++ epi_judge_cpp/sorted_arrays_merge.cc
@@ -1,16 +1,48 @@
+#include <functional>
+#include <queue>
 #include <vector>
 
 #include "test_framework/generic_test.h"
+using std::priority_queue;
 using std::vector;
+
+typedef vector<int>::const_iterator iterator;
+
+struct Result {
+	int value;
+	iterator cur;
+	iterator end;
+};
+
+bool od(Result a, Result b) { return a.value > b.value; }
+
 vector<int> MergeSortedArrays(const vector<vector<int>>& sorted_arrays) {
-  // TODO - you fill in here.
-  return {};
+	priority_queue<Result, vector<Result>, std::function<bool(Result, Result)>> pq(od);
+	/*
+	for (int i = 0; i < sorted_arrays.size(); ++i)
+		pq.push({sorted_arrays[i][0], sorted_arrays[i].begin() + 1, sorted_arrays[i].end()});
+	*/
+	// remember to use reference with for (T item : items)
+	for (const vector<int>& v : sorted_arrays)
+		pq.push({v[0], v.begin() + 1, v.end()});
+	vector<int> ret;
+	while (!pq.empty()) {
+		Result r = pq.top();
+		pq.pop();
+		ret.push_back(r.value);
+		if (r.cur != r.end) {
+			int cur_val = *r.cur;
+			iterator it = r.cur + 1;
+			pq.push({cur_val, it, r.end});
+		}
+	}
+	return ret;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"sorted_arrays"};
-  return GenericTestMain(args, "sorted_arrays_merge.cc",
-                         "sorted_arrays_merge.tsv", &MergeSortedArrays,
-                         DefaultComparator{}, param_names);
+	std::vector<std::string> args{argv + 1, argv + argc};
+	std::vector<std::string> param_names{"sorted_arrays"};
+	return GenericTestMain(args, "sorted_arrays_merge.cc",
+						   "sorted_arrays_merge.tsv", &MergeSortedArrays,
+						   DefaultComparator{}, param_names);
 }
